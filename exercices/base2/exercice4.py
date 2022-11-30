6352cb67488126ff21155acf2779bab5f6634ae3
TRAORE Abdoul-Aziz <traoreabdoulaziz70@gmail.com>
2022-11-30 15:47:50 +0000

    add

--- exercices/base2/exercice4.py
+++ exercices/base2/exercice4.py
@@ -6,13 +6,13 @@ Exemple : la fonction reçoit [1, 1, 2, 4, 9, 2, 5, 4] et retourne [1, 2, 4, 9, 
 
 #J'ai pas mis de parametre car dans notre exmple on a déja une liste definie.
 #Aussi pour éviter qu'a l'appelle de la fonction, je retrouve une retrouve.
-def doublon():   
-    l=[1, 1, 2, 4, 9, 2, 5, 4] 
-    for element in l:
-        if l.count(element)> 1:
-            x=l.index(element)
-            l.pop(x)
-    return l
+def doublon(l=list):
+    m=[]
+    for i in range():
+        if i in l:
+            m.append(i)
+    return m
 
-x=doublon()
+ma_liste = [1, 1, 2, 4, 9, 2, 5, 4]
+x=doublon(ma_liste)
 print(x)
